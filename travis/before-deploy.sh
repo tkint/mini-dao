2c135a0d21c3f04ce8b7c543611a47b1076930a4
Thomas Kint <thomaskint.pro@gmail.com>
2018-03-06 18:03:43 +0100

    Fix travis 1

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_25cf0c002c9c_key -iv $encrypted_25cf0c002c9c_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-    gpg --fast-import travis/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_25cf0c002c9c_key -iv $encrypted_25cf0c002c9c_iv -in codesigning.asc.enc -out codesigning.asc -d
+    gpg --fast-import signingkey.asc
 fi
