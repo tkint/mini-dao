716e330d2bbbc7b3c45a9918b4a3f69df9106b1a
Thomas Kint <thomaskint.pro@gmail.com>
2018-03-06 19:38:59 +0100

    Fix travis 2

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+echo $PWD
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_25cf0c002c9c_key -iv $encrypted_25cf0c002c9c_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import signingkey.asc
+    openssl aes-256-cbc -K $encrypted_25cf0c002c9c_key -iv $encrypted_25cf0c002c9c_iv -in ~/travis/codesigning.asc.enc -out ~/travis/codesigning.asc -d
+    gpg --fast-import ~/travis/codesigning.asc
 fi
--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo $PWD
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy -P sign,build-extras --settings mvnsettings.xml
+    mvn deploy -P sign,build-extras --settings ~/travis/mvnsettings.xml
 fi
