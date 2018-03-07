e3adcd9dde906639d1637d16642eb098aa1afa28
Thomas Kint <thomaskint.pro@gmail.com>
2018-03-07 11:31:39 +0100

    Fix travis (again!)

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -n "$TRAVIS_TAG" ]; then
-    if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
-        echo "Deploying $TRAVIS_TAG"
-        if [ $TRAVIS_BRANCH = 'master' ]; then
-            echo "Deploying production"
-            mvn deploy -P sign,build-extras -s travis/mvnsettings.xml
-        elif [ $TRAVIS_BRANCH = 'develop' ]; then
-            echo "Deploying snapshot"
-            mvn deploy -s travis/mvnsettings.xml
-        fi
-    else
-        echo "Pull request detected: deploy canceled"
-    fi
-else
-    echo "No tag detected: deploy canceled"
+    echo "Deploying $TRAVIS_TAG"
+    mvn deploy -P sign,build-extras -s travis/mvnsettings.xml
+elif [ $TRAVIS_BRANCH = 'develop' ]; then
+    echo "Deploying snapshot"
+    mvn deploy -s travis/mvnsettings.xml
 fi
