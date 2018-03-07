21aa26a22c4131ccd0dfa45cc7e5d76c86d75fa6
Thomas Kint <thomaskint.pro@gmail.com>
2018-03-07 11:11:50 +0100

    Added things

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [[ $TRAVIS_TAG ]]; then
+if [ ${TRAVIS_TAG+x} ]; then
     if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
+        echo "Deploying $TRAVIS_TAG"
         if [ $TRAVIS_BRANCH = 'master' ]; then
-            echo "Deploy master"
+            echo "Deploying production"
             mvn deploy -P sign,build-extras -s travis/mvnsettings.xml
         elif [ $TRAVIS_BRANCH = 'develop' ]; then
-            echo "Deploy snapshot"
+            echo "Deploying snapshot"
             mvn deploy -s travis/mvnsettings.xml
         fi
     else
