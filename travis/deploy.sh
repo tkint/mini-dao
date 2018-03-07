e40bd71dc592fbfb417c6ca147fc13c73f538907
Thomas Kint <tkint@users.noreply.github.com>
2018-03-07 11:06:43 +0100

    Develop (#3)
    * Add some test
    
    * Little patch
    
    * Fix entrypoint (MiniDAO.java) to verify connection
    
    Remove public statement of annotations
    
    * Fix
    
    * Fix javadoc and prepared for maven deployment
    
    * Updated travis configuration
    
    * Added criteria for travis build and deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
-    if [ $TRAVIS_BRANCH = 'master' ]; then
-        mvn deploy -P sign,build-extras -s travis/mvnsettings.xml
-    elif [ $TRAVIS_BRANCH = 'develop' ]; then
-        mvn deploy -s travis/mvnsettings.xml
+if [[ $TRAVIS_TAG ]]; then
+    if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
+        if [ $TRAVIS_BRANCH = 'master' ]; then
+            echo "Deploy master"
+            mvn deploy -P sign,build-extras -s travis/mvnsettings.xml
+        elif [ $TRAVIS_BRANCH = 'develop' ]; then
+            echo "Deploy snapshot"
+            mvn deploy -s travis/mvnsettings.xml
+        fi
+    else
+        echo "Pull request detected: deploy canceled"
     fi
+else
+    echo "No tag detected: deploy canceled"
 fi
