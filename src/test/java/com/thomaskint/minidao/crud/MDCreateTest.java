aaf7afb8aed68edf91b417a3f88314af9c55e804
Thomas Kint <thomaskint.pro@gmail.com>
2018-03-19 16:36:51 +0100

    Add copyright in every files

--- src/test/java/com/thomaskint/minidao/crud/MDCreateTest.java
+++ src/test/java/com/thomaskint/minidao/crud/MDCreateTest.java
@@ -1,6 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Thomas Kint
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.thomaskint.minidao.crud;
 
-import com.thomaskint.minidao.MiniDAO;
 import com.thomaskint.minidao.testonly.model.UserTest;
 import org.junit.Assert;
 import org.junit.Before;
@@ -15,11 +38,11 @@ import static com.thomaskint.minidao.testonly.Config.mdConnectionConfig;
  */
 public class MDCreateTest {
 
-	private MiniDAO miniDAO;
+	private MDCreate create;
 
 	@Before
 	public void init() {
-		miniDAO = new MiniDAO(mdConnectionConfig);
+		create = new MDCreate(mdConnectionConfig);
 	}
 
 	@Test
@@ -30,7 +53,7 @@ public class MDCreateTest {
 
 		boolean created = false;
 		try {
-			created = miniDAO.create().createEntity(userTest);
+			created = create.createEntity(userTest);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
