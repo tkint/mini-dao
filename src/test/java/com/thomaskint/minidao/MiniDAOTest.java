896ef8b1b8811662d268e66f0f3bbcf01475c2c2
Thomas Kint <thomaskint.pro@gmail.com>
2019-10-03 11:13:05 +0200

    Passage à Java 11

--- src/test/java/com/thomaskint/minidao/MiniDAOTest.java
+++ src/test/java/com/thomaskint/minidao/MiniDAOTest.java
@@ -4,6 +4,8 @@ import com.thomaskint.minidao.connection.MDConnection;
 import com.thomaskint.minidao.connection.MDConnectionConfig;
 import com.thomaskint.minidao.connection.MDDriver;
 import com.thomaskint.minidao.exception.MDException;
+import com.thomaskint.minidao.model.MDPair;
+import com.thomaskint.minidao.testonly.Config;
 import com.thomaskint.minidao.testonly.model.UserTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,12 +17,15 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.*;
 
 
@@ -38,10 +43,12 @@ public class MiniDAOTest {
 
 	private final String QUERY = "query";
 
+	private MiniDAO mockedMiniDAO;
 	private UserTest userTest;
 
 	@Before
 	public void init() {
+		mockedMiniDAO = spy(new MiniDAO(Config.connectionConfig));
 		userTest = mock(UserTest.class);
 	}
 
@@ -73,56 +80,64 @@ public class MiniDAOTest {
 	@Test
 	public void should_call_execute_query() throws MDException {
 		// GIVEN
-		MiniDAO miniDAO = new MiniDAO(url, port, login, password, database);
 		ResultSet expectedResultSet = mock(ResultSet.class);
 
 		PowerMockito.mockStatic(MDConnection.class);
 
 		// WHEN
-		Mockito.when(MDConnection.executeQuery(miniDAO.getConnectionConfig(), QUERY)).thenReturn(expectedResultSet);
-		ResultSet resultSet = miniDAO.executeQuery(QUERY);
+		when(MDConnection.executeQuery(mockedMiniDAO.getConnectionConfig(), QUERY)).thenReturn(expectedResultSet);
+		ResultSet resultSet = mockedMiniDAO.executeQuery(QUERY);
 
 		// THEN
-		PowerMockito.verifyStatic(MDConnection.class);
 		assertEquals(resultSet, expectedResultSet);
+	}
+
+	@Test
+	public void should_call_execute_update() throws MDException {
+		// GIVEN
+		Statement statement = mock(Statement.class);
+		MDPair expectedMDPair = new MDPair<>(statement, 0);
+
+		PowerMockito.mockStatic(MDConnection.class);
+
+		// WHEN
+		Mockito.when(MDConnection.executeUpdate(mockedMiniDAO.getConnectionConfig(), QUERY, null)).thenReturn(expectedMDPair);
+		int value = mockedMiniDAO.executeUpdate(QUERY);
+
+		// THEN
+		assertEquals(value, expectedMDPair.getValue());
 	}
 
 	@Test
 	public void should_call_execute_query_and_map_entity() throws MDException {
 		// GIVEN
-		MiniDAO miniDAO = new MiniDAO(url, port, login, password, database);
-		ResultSet expectedResultSet = mock(ResultSet.class);
-
-		MiniDAO mockedMiniDAO = Mockito.spy(miniDAO);
-		PowerMockito.mockStatic(MDConnection.class);
+		ResultSet resultSet = mock(ResultSet.class);
 
 		// WHEN
-		Mockito.when(MDConnection.executeQuery(mockedMiniDAO.getConnectionConfig(), QUERY)).thenReturn(expectedResultSet);
-		Mockito.doReturn(this.userTest).when(mockedMiniDAO).mapResultSetToEntity(expectedResultSet, UserTest.class);
-		UserTest userTest = mockedMiniDAO.executeQueryAndMapToEntity(QUERY, UserTest.class);
+		doReturn(resultSet).when(mockedMiniDAO).executeQuery(QUERY);
+		doReturn(userTest).when(mockedMiniDAO).mapResultSetToEntity(resultSet, UserTest.class);
+		UserTest returnedUserTest = mockedMiniDAO.executeQueryAndMapToEntity(QUERY, UserTest.class);
 
 		// THEN
-		PowerMockito.verifyStatic(MDConnection.class);
-		assertEquals(userTest, this.userTest);
+		verify(mockedMiniDAO, times(1)).executeQuery(QUERY);
+		verify(mockedMiniDAO, times(1)).mapResultSetToEntity(resultSet, UserTest.class);
+		assertEquals(returnedUserTest, userTest);
 	}
 
 	@Test
 	public void should_call_execute_query_and_map_entities() throws MDException {
 		// GIVEN
-		MiniDAO miniDAO = new MiniDAO(url, port, login, password, database);
-		ResultSet expectedResultSet = mock(ResultSet.class);
+		ResultSet resultSet = mock(ResultSet.class);
 		List<UserTest> expectedUserTests = Collections.singletonList(userTest);
 
-		MiniDAO mockedMiniDAO = Mockito.spy(miniDAO);
-		PowerMockito.mockStatic(MDConnection.class);
-
 		// WHEN
-		Mockito.when(MDConnection.executeQuery(mockedMiniDAO.getConnectionConfig(), QUERY)).thenReturn(expectedResultSet);
-		Mockito.doReturn(expectedUserTests).when(mockedMiniDAO).mapResultSetToEntities(expectedResultSet, UserTest.class);
-		List<UserTest> userTests = mockedMiniDAO.executeQueryAndMapToEntities(QUERY, UserTest.class);
+		doReturn(resultSet).when(mockedMiniDAO).executeQuery(QUERY);
+		doReturn(expectedUserTests).when(mockedMiniDAO).mapResultSetToEntities(resultSet, UserTest.class);
+		List<UserTest> returnedUserTests = mockedMiniDAO.executeQueryAndMapToEntities(QUERY, UserTest.class);
 
 		// THEN
-		PowerMockito.verifyStatic(MDConnection.class);
-		assertEquals(userTests, expectedUserTests);
+		verify(mockedMiniDAO, times(1)).executeQuery(QUERY);
+		verify(mockedMiniDAO, times(1)).mapResultSetToEntities(resultSet, UserTest.class);
+		assertEquals(returnedUserTests, expectedUserTests);
 	}
 }
